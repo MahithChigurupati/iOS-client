887917b2b572803e8873392927ec923b8ff52711
Mahith Chigurupati <saimahith.chigurupati@gmail.com>
2023-11-24 14:52:43 -0500

    Initial Commit

---
+++ ZK Wallet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZK Wallet
+//
+//  Created by Mahith ‎ on 11/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
